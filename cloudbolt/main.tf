6ccc095d3cd55b173fa13ad5eee09ee39fcab4b4
Ciaran Cooney <31989641+asysteclab@users.noreply.github.com>
2021-02-24 15:39:40 +0000

    Create main.tf

---
+++ cloudbolt/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04f5641b0d178a27a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
